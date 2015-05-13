1bac6af7b14a7b5bed2148596899eef733ad3a0c
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-05-13 15:00:58 -0700

    swap timer0 for symbol counter

--- avr/cores/pinoccio/wiring.c
+++ avr/cores/pinoccio/wiring.c
@@ -24,86 +24,38 @@
 
 #include "wiring_private.h"
 
-// the prescaler is set so that timer0 ticks every 64 clock cycles, and the
-// the overflow handler is called every 256 ticks.
-#define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(64 * 256))
-
-// the whole number of milliseconds per timer0 overflow
-#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
-
-// the fractional number of milliseconds per timer0 overflow. we shift right
-// by three to fit these numbers into a byte. (for the clock speeds we care
-// about - 8 and 16 MHz - this doesn't lose precision.)
-#define FRACT_INC ((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
-#define FRACT_MAX (1000 >> 3)
+static const uint8_t US_PER_TICK = (1000000/62500);
 
 volatile unsigned long timer0_overflow_count = 0;
-volatile unsigned long timer0_millis = 0;
-static unsigned char timer0_fract = 0;
 
-#if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-ISR(TIM0_OVF_vect)
-#else
-ISR(TIMER0_OVF_vect)
-#endif
-{
-    // copy these to local variables so they can be stored in registers
-    // (volatile variables must be read from memory on every access)
-    unsigned long m = timer0_millis;
-    unsigned char f = timer0_fract;
+#define MICROSECONDS_PER_TIMER0_OVERFLOW (4294967296 * 16)
 
-    m += MILLIS_INC;
-    f += FRACT_INC;
-    if (f >= FRACT_MAX) {
-        f -= FRACT_MAX;
-        m += 1;
-    }
-
-    timer0_fract = f;
-    timer0_millis = m;
+ISR(SCNT_OVFL_vect) {
     timer0_overflow_count++;
+}
+
+uint32_t read_sccnt() {
+    // Read LL first, that will freeze the other registers for reading
+    uint32_t sccnt = SCCNTLL;
+    sccnt |= (uint32_t)SCCNTLH << 8;
+    sccnt |= (uint32_t)SCCNTHL << 16;
+    sccnt |= (uint32_t)SCCNTHH << 24;
+    return sccnt;
+}
+
+unsigned long micros()
+{
+    return (uint64_t)read_sccnt() * US_PER_TICK;
 }
 
 unsigned long millis()
 {
     unsigned long m;
-    uint8_t oldSREG = SREG;
-
-    // disable interrupts while we read timer0_millis or we might get an
-    // inconsistent value (e.g. in the middle of a write to timer0_millis)
-    cli();
-    m = timer0_millis;
-    SREG = oldSREG;
-
-    return m;
-}
-
-unsigned long micros() {
-    unsigned long m;
-    uint8_t oldSREG = SREG, t;
-    
-    cli();
+    SCIRQM &= ~(1 << IRQMOF);
     m = timer0_overflow_count;
-#if defined(TCNT0)
-    t = TCNT0;
-#elif defined(TCNT0L)
-    t = TCNT0L;
-#else
-    #error TIMER 0 not defined
-#endif
+    SCIRQM |= (1 << IRQMOF);
 
-  
-#ifdef TIFR0
-    if ((TIFR0 & _BV(TOV0)) && (t < 255))
-        m++;
-#else
-    if ((TIFR & _BV(TOV0)) && (t < 255))
-        m++;
-#endif
-
-    SREG = oldSREG;
-    
-    return ((m << 8) + t) * (64 / clockCyclesPerMicrosecond());
+    return m * MICROSECONDS_PER_TIMER0_OVERFLOW + micros()/1000;
 }
 
 void delay(unsigned long ms)
@@ -189,46 +141,22 @@ void delayMicroseconds(unsigned int us)
 
 void init()
 {
+
+    // Enable asynchronous mode for timer2. This is required to start the
+    // 32kiHz crystal at all, so we can use it for the symbol counter. See
+    // http://www.avrfreaks.net/index.php?name=PNphpBB2&file=viewtopic&t=142962
+    ASSR |= (1 << AS2);
+
+    // Enable the symbol counter, using the external 32kHz crystal
+    // SCCR1 is left at defaults, with CMP3 in absolute compare mode.
+    SCCR0 |= (1 << SCEN) | (1 << SCCKSEL);
+
+    // Enable the SCNT_OVFL interrupt for timekeeping
+    SCIRQM |= (1 << IRQMOF);
+
     // this needs to be called before setup() or some functions won't
     // work there
     sei();
-    
-    // on the ATmega168, timer 0 is also used for fast hardware pwm
-    // (using phase-correct PWM would mean that timer 0 overflowed half as often
-    // resulting in different millis() behavior on the ATmega8 and ATmega168)
-#if defined(TCCR0A) && defined(WGM01)
-    sbi(TCCR0A, WGM01);
-    sbi(TCCR0A, WGM00);
-#endif  
-
-    // set timer 0 prescale factor to 64
-#if defined(__AVR_ATmega128__)
-    // CPU specific: different values for the ATmega128
-    sbi(TCCR0, CS02);
-#elif defined(TCCR0) && defined(CS01) && defined(CS00)
-    // this combination is for the standard atmega8
-    sbi(TCCR0, CS01);
-    sbi(TCCR0, CS00);
-#elif defined(TCCR0B) && defined(CS01) && defined(CS00)
-    // this combination is for the standard 168/328/1280/2560
-    sbi(TCCR0B, CS01);
-    sbi(TCCR0B, CS00);
-#elif defined(TCCR0A) && defined(CS01) && defined(CS00)
-    // this combination is for the __AVR_ATmega645__ series
-    sbi(TCCR0A, CS01);
-    sbi(TCCR0A, CS00);
-#else
-    #error Timer 0 prescale factor 64 not set correctly
-#endif
-
-    // enable timer 0 overflow interrupt
-#if defined(TIMSK) && defined(TOIE0)
-    sbi(TIMSK, TOIE0);
-#elif defined(TIMSK0) && defined(TOIE0)
-    sbi(TIMSK0, TOIE0);
-#else
-    #error  Timer 0 overflow interrupt not set correctly
-#endif
 
     // timers 1 and 2 are used for phase-correct hardware pwm
     // this is better for motors as it ensures an even waveform
